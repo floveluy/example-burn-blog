55b4c3b914b70cdeeb302114d076b7eaf6fba9db
Z F <215566435@qq.com>
2018-02-07 11:05:09 +1300

    ➕博客crud完毕

--- src/controller/index.ts
+++ src/controller/index.ts
@@ -1,28 +1,48 @@
 import { Controller, Blueprint } from 'burnjs';
 
 
-@Blueprint.restfulClass('/article/:id')
+@Blueprint.restfulClass('/article')
 export default class Index extends Controller {
     async insertModel(name: string) {
         console.log(this.ctx.body)
         await this.ctx.model[name].create(this.ctx.request.body);
     }
 
-    async Get() {
-        this.ctx.body = this.ctx.params.id;
+    @Blueprint.get('/article/:id')
+    async getArticle() {
+        const d: any | null = await this.ctx.model.article.findOne({
+            where: {
+                articleID: this.ctx.params.id
+            }
+        })
+        if (d) {
+            this.ctx.body = JSON.stringify(d);
+        }
     }
 
     async Post() {
-        this.ctx.body = this.ctx.params.id;
         await this.insertModel('article');
     }
 
     async Del() {
+        const id = this.ctx.params.id;
 
+        this.ctx.model.article.destroy({
+            where: {
+                id: id
+            }
+        })
     }
 
     async Put() {
-
+        this.ctx.model.article.update({
+            content: this.ctx.request.body.content,
+            title: this.ctx.request.body.title
+        }, {
+                where: {
+                    articleId: this.ctx.request.body.articleId
+                }
+            })
     }
 }
 
